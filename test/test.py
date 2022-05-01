39ea7bb42da8a524d94d0e18e516fa5f8d29f4ef
Oleksandr Draha <oleksandr.draha@takeoff.com>
2022-05-01 18:35:06 +0300

    Add test python file

---
+++ test/test.py
@@ -0,0 +1,13 @@
+
+
+import sys
+
+
+
+
+def bAdName(s):
+    a= s+1
+    return "Let's imagine that this is a real long line, but for truth saying, I don't have nothing about this line length, sorry Guido"
+
+
+bAdName(2)
