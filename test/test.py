45a245eb13e3d27e7f823760c51bd36549f2d275
Oleksandr Draha <oleksandr.draha@takeoff.com>
2022-05-11 22:20:26 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ import sys
 
 
 def bAdName(s):
-    a= s+1     - 23
+    a= s+1     - 234
     b =            a
     return "Let's imagine that this is a real long line, but for truth saying, I don't have nothing about this line length, sorry Guido"
 
