87a84fc105653eab4d8a7b70684eb527c6fffd8a
Oleksandr Draha <oleksandr.draha@takeoff.com>
2022-05-11 23:19:42 +0300

    rrr

--- test/test.py
+++ test/test.py
@@ -1,16 +1,10 @@
-
-
 import sys
 
 
-
-
 def bAdName(s):
-    a= s+1     - 2345
-    b =            a
-    return "Let's imagine that this is a real long line, but for truth saying, I don't have nothing about this line length, sorry Guido"
+    a = s + 1 - 2345
+    b = a
+    return b, "Let's imagine that this is a real long line, but for truth saying, I don't have nothing about this line length, sorry Guido"
 
 
 bAdName(2)
-
-
