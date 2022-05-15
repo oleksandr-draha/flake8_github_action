367dd35a8b5bcf38e21adf832d5b50bf62ec9aff
Oleksandr Draha <oleksandr.draha@takeoff.com>
2022-05-15 14:49:03 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 def bAdName(s):
-    a=s + 1 -             2345
+    a=s + 1 -          2345
     b=a
     return b, "Let's imagine that this is a real long line, but for truth saying, I don't have nothing about this line length, sorry Guido"
 
